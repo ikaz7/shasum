4468a0fa755dec57cca5a127810089573d871d56
Zaki Ghouse <zghouse7@gmail.com>
2020-07-21 20:58:56 -0500

    make checksum concurrent

--- main.go
+++ main.go
@@ -15,21 +15,29 @@ var s384 = flag.Bool("384", false, "print sha384")
 func main() {
 	flag.Parse()
 	input := flag.Args()
-	for _, f := range input {
-		b, err := ioutil.ReadFile(f)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "shasum: %v\n", err)
-		}
-		switch {
-		case *s512:
-			digest := sha512.Sum512(b)
-			fmt.Printf("%x\t%s\n", digest, f)
-		case *s384:
-			digest := sha512.Sum384(b)
-			fmt.Printf("%x\t%s\n", digest, f)
-		default:
-			digest := sha256.Sum256(b)
-			fmt.Printf("%x\t%s\n", digest, f)
-		}
+	ch := make(chan struct{})
+	for _, file := range input {
+		go func(f string) {
+			b, err := ioutil.ReadFile(f)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "shasum: %v\n", err)
+				os.Exit(1)
+			}
+			switch {
+			case *s512:
+				digest := sha512.Sum512(b)
+				fmt.Printf("%x\t%s\n", digest, f)
+			case *s384:
+				digest := sha512.Sum384(b)
+				fmt.Printf("%x\t%s\n", digest, f)
+			default:
+				digest := sha256.Sum256(b)
+				fmt.Printf("%x\t%s\n", digest, f)
+			}
+			ch <- struct{}{}
+		}(file)
+	}
+	for range input {
+		<-ch
 	}
 }
